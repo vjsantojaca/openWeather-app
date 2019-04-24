b65ce4d3932890aaf99550907374e946c2052da8
Víctor (vjsantojaca) <victorsantoja@gmail.com>
2019-04-24 20:41:00 +0200

    Real Initial Commit: * App.kt * MainActivity.kt
    Added dependencies Rx, dagger and retrofit

---
+++ app/src/main/java/com/vjsantojaca/openweather/App.kt
@@ -0,0 +1,11 @@
+package com.vjsantojaca.openweather
+
+import android.app.Application
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
---
+++ app/src/main/java/com/vjsantojaca/openweather/ui/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vjsantojaca.openweather.ui.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.vjsantojaca.openweather.openweatherapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
