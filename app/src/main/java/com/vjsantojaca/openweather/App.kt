725948c9c2671e2492c7860c0b37bc2c4c1da63a
Víctor (vjsantojaca) <victorsantoja@gmail.com>
2019-04-25 19:23:16 +0200

    Fixed dependency dagger problems

--- app/src/main/java/com/vjsantojaca/openweather/App.kt
+++ app/src/main/java/com/vjsantojaca/openweather/App.kt
@@ -1,17 +1,14 @@
 package com.vjsantojaca.openweather
 
 import android.app.Application
+
 import com.vjsantojaca.openweather.di.components.ApplicationComponent
 import com.vjsantojaca.openweather.di.components.DaggerApplicationComponent
-import com.vjsantojaca.openweather.di.modules.ApplicationModule
 
 class App : Application() {
 
     val component: ApplicationComponent by lazy {
-        DaggerApplicationComponent
-            .builder()
-            .applicationModule(ApplicationModule(this))
-            .build()
+        DaggerApplicationComponent.create()
     }
 
     override fun onCreate() {
